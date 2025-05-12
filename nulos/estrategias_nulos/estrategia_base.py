b0aa103c1ed4bbb2f37b6843dfb6c3b2291c6b87
HugoGarabatos23 <hugo.garabatos.udc.es>
2025-05-13 01:49:40 +0200

    refactor: pep8 style

--- nulos/estrategias_nulos/estrategia_base.py
+++ nulos/estrategias_nulos/estrategia_base.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 
+
 class EstrategiaNulos(ABC):
     @abstractmethod
     def aplicar(self, df: pd.DataFrame, columnas: list) -> pd.DataFrame:
--- selector_columnas/selector_columnas.py
+++ selector_columnas/selector_columnas.py
@@ -1,5 +1,6 @@
 from estado import AppState
-import re 
+import re
+
 
 def mostrar_submenu_seleccion_columnas(estado: AppState) -> None:
     if not estado.datos_cargados():
@@ -11,43 +12,46 @@ def mostrar_submenu_seleccion_columnas(estado: AppState) -> None:
     print("Selección de Columnas")
     print("=============================")
     print("Columnas disponibles en los datos:")
-    
+
     columnas = list(estado.datos.columns)
     for i, col in enumerate(columnas, 1):
         print(f"  [{i}] {col}")
-    
+
     # Solicitar al usuario que ingrese las columnas de entrada (features)
     # Repetimos hasta que el usuario ingrese features y target válidos
     correccion = False
     while not correccion:
-    
-        features_input = input("\nIngrese los números de las columnas de entrada (features), separados por comas: ").strip()
+
+        features_input = input(
+            "\nIngrese los números de las columnas de entrada (features), separados por comas: ").strip()
         # Verificamos que la entrada contenga solo números, comas y espacios
-                # Validar con regex
+        # Validar con regex
         if not re.fullmatch(r"[0-9,\s]+", features_input):
             print("❌ Error: Use solo números separados por comas. Ejemplo: 1,2,3")
             continue
-        
+
         try:
-            features_indices = [int(i.strip()) - 1 for i in features_input.split(",")]
+            features_indices = [
+                int(i.strip()) - 1 for i in features_input.split(",")]
             if any(i < 0 or i >= len(columnas) for i in features_indices):
                 print(" ❌Error: Uno o más índices están fuera de rango.")
                 continue
-        
-        
+
             features = [columnas[i] for i in features_indices]
 
             if not features:
                 print("⚠ Error: Debe seleccionar al menos una columna como feature.")
                 continue
-        
+
             # Solicitar la columna de salida (target)
            # Solicitar la columna de salida (target)
-            target_input = input("\nIngrese el número de la columna de salida (target): ").strip()
+            target_input = input(
+                "\nIngrese el número de la columna de salida (target): ").strip()
 
             # Validar que sea un único número
             if not re.fullmatch(r"\d+", target_input):
-                print("❌ Error: Solo se permite una única columna como target (un número).")
+                print(
+                    "❌ Error: Solo se permite una única columna como target (un número).")
                 continue
 
             target_index = int(target_input) - 1
@@ -55,12 +59,12 @@ def mostrar_submenu_seleccion_columnas(estado: AppState) -> None:
             if target_index < 0 or target_index >= len(columnas):
                 print("❌ Error: Índice fuera de rango.")
                 continue
-            
-            
+
             target = columnas[target_index]
 
             if target in features:
-                print("⚠ Error: La columna de salida (target) no puede estar entre las features.")
+                print(
+                    "⚠ Error: La columna de salida (target) no puede estar entre las features.")
                 continue
 
             # Almacenar selección
@@ -68,8 +72,9 @@ def mostrar_submenu_seleccion_columnas(estado: AppState) -> None:
             estado.target = target
 
             # Confirmar selección
-            print(f"\nSelección guardada: Features = {estado.features}, Target = {estado.target}")
-            
+            print(
+                f"\nSelección guardada: Features = {estado.features}, Target = {estado.target}")
+
             # Actualizar estado del menú principal
             estado.estado_columnas_seleccionadas = True
             correccion = True
