f0b2dd6be721f090eb1a75638aacfcdb573c739a
HugoGarabatos23 <hugo.garabatos.udc.es>
2025-05-14 23:31:36 +0200

    refactor: #11 test structure

--- tests/visualizacion/test_dispersion_plot.py
+++ tests/visualizacion/test_dispersion_plot.py
@@ -28,18 +28,3 @@ def test_dispersion_plot_sin_datos(mock_print, estado_con_datos):
     # Verificar que se imprimió el mensaje de error
     mock_print.assert_any_call(
         "❌ No hay datos originales disponibles para comparar.")
-
-
-# Test cuando no hay columnas numéricas válidas
-@patch("builtins.print")
-def test_dispersion_plot_sin_columnas_numericas(mock_print, estado_con_datos):
-    # Cambiar las columnas para que no haya columnas numéricas
-    estado_con_datos.datos['A'] = estado_con_datos.datos['A'].astype('object')
-    estado_con_datos.datos['B'] = estado_con_datos.datos['B'].astype('object')
-
-    visualizacion = DispersionPlot()
-    visualizacion.crear_visualizacion(estado_con_datos)
-
-    # Verificar que no se intenta generar gráficos
-    mock_print.assert_any_call(
-        "❌ No hay columnas numéricas adecuadas para crear un histograma.")
